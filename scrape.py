ef09c193b80b0f5a0440378a6ccd93e5b809d302
occupi2011 <44349404+occupi2011@users.noreply.github.com>
2018-12-10 20:20:23 -0800

    Updating scrape.py for python3

--- scrape.py
+++ scrape.py
@@ -14,9 +14,12 @@ import re
 import time
 import json
 import urllib
-import urllib2
+from urllib.parse import urlencode
+from urllib.request import urlopen
+from urllib.error import HTTPError
 
 def is_interesting(data):
+    data = str(data)
     """Determine if data contains any interesting artifacts"""
     # TODO:
     # - phone numbers
@@ -28,14 +31,22 @@ def is_interesting(data):
     # - User-specified keywords
     # - Display blurb on WHY the file is interesting in output:
     #   ex: [+] Interesting data found in %s -- saved to %s (exploit)
-    if 'exploit' in data.lower():
+    find_username = re.compile("^[a-zA-Z]+\s[0-9]+\s[0-9:]{8}\s<([A-Za-z]+)>", re.MULTILINE)
+    found_usernames = find_username.findall(data.lower())
+    if found_usernames:
         return True
-    if 'pass' in data.lower():
-        return True
-    if 'key' in data.lower():
-        return True
-    if 'database' in data.lower():
-        return True
+    finduseraction = re.compile("^[a-zA-Z]+\s[0-9]+\s[0-9:]{8}\s\*\s+([A-Za-z]+)\s", re.MULTILINE)
+    found_useractions = finduseraction.findall(data.lower())
+    if found_useractions:
+        return True   
+    #if 'exploit' in data.lower():
+    #    return True
+    #if 'pass' in data.lower():
+    #    return True
+    #if 'key' in data.lower():
+    #    return True
+    #if 'database' in data.lower():
+    #    return True
     return False
 
 
@@ -52,18 +63,18 @@ def main():
     pastebin_keys = []
     pastebin = ""
     limit = 500  # TODO: CLI setting
-    url = "http://pastebin.com/api_scraping.php"
+    url = "https://scrape.pastebin.com/api_scraping.php"
     values = {'limit': limit}
-    url_values = urllib.urlencode(values)
+    url_values = urlencode(values)
 
     full_url = url + '?' + url_values
 
     while True:
         # TODO: exit if IP is not whitelisted.
         try:
-            pastebin = urllib2.urlopen(full_url)
-        except urllib2.HTTPError, e:
-            print e
+            pastebin = urlopen(full_url)
+        except HTTPError as e:
+            print(e)
 
         data = json.load(pastebin)
         pastebin_keys = pastebin_keys[:limit]
@@ -74,17 +85,17 @@ def main():
             pastebin_keys.insert(0, paste['key'])
             #print paste['key'], paste['date'], paste['scrape_url'], paste['full_url']
             # TODO: add exception handling
-            scrape = urllib2.urlopen(paste['scrape_url'])
+            scrape = urlopen(paste['scrape_url'])
             scrape_data = scrape.read()
             if is_interesting(scrape_data):
                 filename = paste['key'] + ".txt"
-                print "[+] Interesting data found in %s -- saving to %s" % \
-                    (paste['full_url'], filename)
+                print("[+] Interesting data found in %s -- saving to %s" % \
+                    (paste['full_url'], filename))
 
                 # TODO: make sure this succeeds
                 filep = open(filename, 'w')
                 # TODO: ability to specify write directory, folders by date.
-                filep.write(scrape_data)
+                filep.write(str(scrape_data))
                 filep.close()
                 
 
